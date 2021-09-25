eadb65b5ad43294468ca263b4aad244ca262ac8c
Nota30 <harenkeshaav11@gmail.com>
2021-09-25 18:57:19 +0600

    add host

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,6 +3,7 @@ import users from './Schemas/Users';
 
 const { DATABASE_PASSWORD, USER, DATABASE } = process.env as { [x: string]: string };
 const sequelize = new Sequelize(DATABASE, USER, DATABASE_PASSWORD, {
+    host: 'localhost',
     dialect: 'postgres',
     logging: false
 });
