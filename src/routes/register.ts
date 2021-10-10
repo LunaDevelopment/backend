6817714f66cfa4d01b55c5f3e597af96c53d0f1c
Nota30 <harenkeshaav11@gmail.com>
2021-10-10 11:58:22 +0600

    changes

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -1,55 +1,42 @@
 import { Router, Response } from '.';
-import * as jwt from 'jsonwebtoken';
 import argon2 from 'argon2';
 
 const router = Router();
 
 router.post('/', async (req, res: Response) => {
     try {
-        if (req.headers['authorization']) {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            const recieved: any = jwt.verify(req.headers['authorization'], process.env.JWT_SECRET as string);
-            if (!recieved.email) {
-                return res.status(401).send({
-                    message: 'token authentication failure'
-                });
-            }
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        console.log(req);
+        const { email, username, password } = req.body;
+        const dbrq = await res.locals.Users.findOne({
+            where: { email }
+        });
 
-            const { email, username, password } = recieved as { [x: string]: string };
-            const dbrq = await res.locals.Users.findOne({
-                where: { email }
-            });
-
-            if (dbrq) {
-                return res.status(401).send({
-                    message: 'email already exists'
-                });
-            }
-
-            const userRQ = await res.locals.Users.findOne({
-                where: { username }
-            });
-
-            if (userRQ) {
-                return res.status(401).send({
-                    message: 'username already exists'
-                });
-            }
-
-            if (!dbrq) {
-                const hash = await argon2.hash(password);
-                await res.locals.Users.create({
-                    email,
-                    username,
-                    password: hash
-                });
-                res.status(201).send({
-                    message: 'account created'
-                });
-            }
-        } else {
+        if (dbrq) {
             return res.status(401).send({
-                message: 'no token'
+                message: 'email already exists'
+            });
+        }
+
+        const userRQ = await res.locals.Users.findOne({
+            where: { username }
+        });
+
+        if (userRQ) {
+            return res.status(401).send({
+                message: 'username already exists'
+            });
+        }
+
+        if (!dbrq) {
+            const hash = await argon2.hash(password);
+            await res.locals.Users.create({
+                email,
+                username,
+                password: hash
+            });
+            res.status(201).send({
+                message: 'account created'
             });
         }
     } catch (error) {
