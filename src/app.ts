a3cff507f65900e1e4a28a27eee2ca29813aec12
Nota <harenkeshaav11@gmail.com>
2021-10-14 03:35:09 +0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -21,14 +21,7 @@ const redisClient = new Redis();
 const allowedOrigins = ['https://moonhideoutdev.com', 'http://localhost:8080'];
 app.use(
     cors({
-        origin: function (origin, callback) {
-            if (!origin) return callback(null, true);
-            if (allowedOrigins.indexOf(origin) === -1) {
-                const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-                return callback(new Error(msg), false);
-            }
-            return callback(null, true);
-        },
+        origin: allowedOrigins,
         credentials: true,
         exposedHeaders: ['Set-Cookie']
     })
