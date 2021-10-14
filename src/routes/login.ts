762609142a96bbd6a32ca3537357626aa682eac4
Nota <harenkeshaav11@gmail.com>
2021-10-14 19:03:18 +0600

    Update login.ts

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -6,6 +6,8 @@ const router = Router();
 router.get('/discord', passport.authenticate('discord'));
 
 router.get('/auth', (req, res) => {
+    const c = req.cookies['discord.oauth'];
+    res.cookie('discord.oauth', c, { maxAge: 60000 * 60 * 24 * 7, httpOnly: true, sameSite: 'none', secure: true });
     console.log(req.user);
     res.send(req.user);
 });
