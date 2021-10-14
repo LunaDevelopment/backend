8f01e124f9cee968e3c755825bde0057e5b5f780
Nota30 <harenkeshaav11@gmail.com>
2021-10-14 18:40:46 +0600

    change

--- src/app.ts
+++ src/app.ts
@@ -6,17 +6,17 @@ import createError from 'http-errors';
 import { Models } from './models';
 import logger from 'morgan';
 import passport from 'passport';
-import session from 'express-session';
+// import session from 'express-session';
 import './strategies/discordOauth';
 import cors from 'cors';
 //import compression from 'compression';
-import Redis from 'ioredis';
-import connectRedis from 'connect-redis';
+// import Redis from 'ioredis';
+// import connectRedis from 'connect-redis';
 import cookieSession from 'cookie-session';
 
 const app = express();
-const RedisStore = connectRedis(session);
-const redisClient = new Redis();
+// const RedisStore = connectRedis(session);
+// const redisClient = new Redis();
 
 const allowedOrigins = ['https://moonhideoutdev.com', 'http://localhost:8080'];
 app.use(
@@ -37,20 +37,20 @@ app.use(
     })
 );
 
-app.use(
-    session({
-        cookie: {
-            maxAge: 60000 * 60 * 24 * 7,
-            secure: true,
-            sameSite: 'none'
-        },
-        secret: 'nvdbuw93090rei-f09dsju4b',
-        resave: false,
-        saveUninitialized: false,
-        store: new RedisStore({ client: redisClient }),
-        name: 'discord.oauth'
-    })
-);
+// app.use(
+//     session({
+//         cookie: {
+//             maxAge: 60000 * 60 * 24 * 7,
+//             secure: true,
+//             sameSite: 'none'
+//         },
+//         secret: 'nvdbuw93090rei-f09dsju4b',
+//         resave: false,
+//         saveUninitialized: false,
+//         store: new RedisStore({ client: redisClient }),
+//         name: 'discord.oauth'
+//     })
+// );
 
 // app.use(
 //     compression({
