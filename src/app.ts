197dd72d4d1463055fa19d9d72e746586bca2f33
Nota30 <harenkeshaav11@gmail.com>
2021-10-14 21:52:10 +0600

    change

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.use(
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Origin', 'https://web.postman.co');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
